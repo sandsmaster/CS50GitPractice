e2f20ea5c1764a6d2fd5721ed83d1429c4af0228
sandsmaster <chichkoliobopitko@gmail.com>
2026-03-29 07:27:29 +0200

    Add Template

--- DJPractice/testapp/views.py
+++ DJPractice/testapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello !")
+    return render(request, "testapp/index.html")
 
 def greet(request, name):
     return HttpResponse(f"Hello {name}!")
