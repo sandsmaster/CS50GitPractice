4dbefb6ec289f58e736dc03ae58dd8c7713b43dd
sandsmaster <chichkoliobopitko@gmail.com>
2026-03-30 03:34:48 +0200

    Add Generic Form

--- DJPractice/tasks/views.py
+++ DJPractice/tasks/views.py
@@ -1,4 +1,9 @@
+from django import forms
 from django.shortcuts import render
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 tasks = ["Clean Fridge", "Clean Bathroom", "Shave Mustache"]
 # Create your views here.
@@ -8,4 +13,6 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
